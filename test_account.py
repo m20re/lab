ada99815afa06453b9b4b4f3d0e6129488b9b32f
m20re <marvinorellana@unomaha.edu>
2023-04-20 18:00:16 -0500

    Added more tests for test_withdraw

--- test_account.py
+++ test_account.py
@@ -9,7 +9,7 @@ def test_init():
     assert testAcccount.get_name() == 'Noa'
 
 def test_deposit():
-    # checking boolean logic - int
+    # checking boolean return - int
     testAccount = Account('John')
     assert testAccount.deposit(45) is True
     assert testAccount.deposit(0) is False
@@ -20,7 +20,7 @@ def test_deposit():
     assert testAccount.get_balance() == 100
     testAccount.deposit(-1)
     assert testAccount.get_balance() == 100
-    # checking boolean logic - float
+    # checking boolean return - float
     assert testAccount.deposit(45.0) is True
     assert testAccount.deposit(0.0) is False
     assert testAccount.deposit(-1.0) is False
@@ -30,18 +30,22 @@ def test_deposit():
     testAccount.deposit(5.5)
     assert testAccount.get_balance() == pytest.approx(151.6, abs=0.001)
     testAccount.deposit(-1.1)
-    assert testAccount.get_balance == pytest.approx(151.6, abs=0.001)
+    assert testAccount.get_balance() == pytest.approx(151.6, abs=0.001)
+    # checking string values
+    assert testAccount.deposit('Obama') is False
+    testAccount.deposit('Obama')
+    assert testAccount.get_balance() == pytest.approx(151.6, abs=0.001)
 
     
 
 def test_withdraw():
     testAccount = Account('John')
+    # checking boolean return - int
     assert testAccount.withdraw(0) is False
     testAccount.deposit(55)
     assert testAccount.withdraw(56) is False
-    assert testAccount.withdraw(5) is True
-    assert testAccount.withdraw(50) is True
-    #testing numerical values
+    assert testAccount.withdraw(55) is True
+    # checking numerical values - int
     testAccount.deposit(100)
     testAccount.withdraw(50)
     assert testAccount.get_balance() == 50
@@ -49,3 +53,9 @@ def test_withdraw():
     assert testAccount.get_balance() == 0
     testAccount.withdraw(-1)
     assert testAccount.get_balance() == 0
+    # checking boolean return - float
+    testAccount.deposit(50.0)
+    assert testAccount.withdraw(50.0) is True
+    assert testAccount.withdraw(0.0) is False
+    assert testAccount.withdraw(-1.1) is False
+    
